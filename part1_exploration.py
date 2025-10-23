9ced4f3136bcfe0435b59d94e8b485aa2daa49fe
PQ <220984062+paulquimbo@users.noreply.github.com>
2025-10-23 09:58:29 -0400

    Modify SQL query and add metric analysis
    Updated SQL query limit and commented out data saving code. Added duplicate removal and unique count for 'metric' column.

--- part1_exploration.py
+++ part1_exploration.py
@@ -19,23 +19,31 @@ url_string = f"mysql+pymysql://{sql_username}:{sql_password}@{sql_host}:3306/{sq
 
 conn = create_engine(url_string)
 
-# remove limit 50; to get full dataset
+# remove limit 1000; to get full dataset
 sql_toexecute = """
   select *
   from research_experiment_refactor_test
-  limit 50; 
+  limit 1000000; 
   """
 
 response = pd.read_sql(sql_toexecute, conn)
-response
 
-## Downloading the data locally
+## Downloading the data locally (optional) remove # below to enable
 # Ensure 'raw' folder exists
-raw_folder = "raw"
-os.makedirs(raw_folder, exist_ok=True)
+# raw_folder = "raw"
+# os.makedirs(raw_folder, exist_ok=True)
 
-#Save result to CSV in 'raw' folder
-output_path = os.path.join(raw_folder, "query_result.csv")
-response.to_csv(output_path, index=False)
+##Save result to CSV in 'raw' folder
+# output_path = os.path.join(raw_folder, "query_result.csv")
+# response.to_csv(output_path, index=False)
+# print(f"Query result saved to: {output_path}")
 
-print(f"Query result saved to: {output_path}")
+# removing duplicate rows based even if ID number is different
+response_duprem = response.drop_duplicates(subset=[col for col in response.columns if col != 'ID'])
+
+# viewing list of entries in metric column
+metric_list = response_duprem['metric'].unique()
+
+# counting unique entries in metric column
+unique_count = metric_list.shape[0]
+print(unique_count)
